0c124576bfe4fba509d973e925f7fabfaab5e307
yaba12 <31941014+yaba12@users.noreply.github.com>
2018-04-26 12:14:16 -0500

    Update BackGround.cpp

--- src/BackGround.cpp
+++ src/BackGround.cpp
@@ -4,34 +4,32 @@
 BackGround::BackGround(int row, int col) {
 	rows = row;
 	cols = col;
-	for (int i = 0; i < row; i++) {
+	for (int x_index = 0; x_index < row; x_index++) {
 		std::vector<Tile> row;
-		for (int j = 0; j < col; j++) {
-			row.push_back(Tile(i * height, j * width, ofColor::orange));
+		for (int y_index = 0; y_index < col; y_index++) {
+			row.push_back(Tile(x_index * height, y_index * width, ofColor::orange));
 		}
 		background_tile.push_back(row);
 	}
 }
 
-	void BackGround::draw() {
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				background_tile[i][j].draw();
+void BackGround::draw() {
+	for (int x_index = 0; x_index < rows; x_index++) {
+		for (int y_index = 0; y_index < cols; y_index++) {
+			background_tile[x_index][y_index].draw();
+		}
+	}
+}
+
+void BackGround::delete_row(int row) {
+	for (int x_index = row; x_index >= 0; x_index++) {
+		for (int y_index = 0; y_index < cols; y_index++) {
+			if (x_index - 1 >= 0) {
+				background_tile[y_index][x_index].stroke = background_tile[y_index - 1][x_index].stroke;
+			}
+			else {
+				background_tile[y_index][0].stroke = ofColor::orange;
 			}
 		}
 	}
-	
-	void BackGround::delete_bottom_row() {
-		std::vector<std::vector<Tile>> temp_other;
-		for (int i = 0; i < rows; i++) {
-			std::vector<Tile> temp_row;
-			for (int j = 0; j < cols; j++) {
-				if (i == rows - 1) {
-					temp_row.push_back(Tile(i * height, j * width, ofColor::orange));
-				}	
-				else {
-					temp_row.push_back(background_tile[i][j]);
-				}
-				temp_other.push_back(temp_row);
-			}
-		}
+}
