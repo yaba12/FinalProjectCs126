77cf6c5e5bbb19050bb032880c3e8a8e82f2015a
yaba12 <31941014+yaba12@users.noreply.github.com>
2018-04-26 12:19:13 -0500

    Update Shape.h

--- src/Shape.h
+++ src/Shape.h
@@ -5,7 +5,16 @@
 class Shape {
 public:
 	std::vector<std::vector<std::vector<Tile>>> different_shapes;
+	
+	//Make the Shapes and add to vector
 	void CreateShapes();
-	std::vector < std::vector<Tile>> get_random_shape();
+	void CreateSquare();
+	void CreateT();
+	void CreateL();
+	void CreateLowerCasel();
+	void CreateS();
+	void CreateZ();
+	void CreateJ();
+	std::vector < std::vector<Tile>> GetRandomShape();
 	void draw(std::vector<std::vector<Tile>> shape);
 };
