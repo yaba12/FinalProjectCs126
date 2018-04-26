4e25796ef7f3a64e617cd73f886c80cb9d752a76
yaba12 <31941014+yaba12@users.noreply.github.com>
2018-04-26 12:18:54 -0500

    Update Shape.cpp

--- src/Shape.cpp
+++ src/Shape.cpp
@@ -5,8 +5,16 @@
 #include <vector>
 
 void Shape::CreateShapes() {
+	CreateSquare();
+	CreateT();
+	CreateL();
+	CreateLowerCasel();
+	CreateS();
+	CreateZ();
+	CreateJ();
+}
+	void CreateSquare(){
 	std::vector<std::vector<Tile>> square_shape;
-
 	std::vector<Tile> half_square;
 	std::vector<Tile> other_half;
 	half_square.push_back(Tile(0, 0, ofColor::yellow));
@@ -16,17 +24,10 @@ void Shape::CreateShapes() {
 	square_shape.push_back(half_square);
 	square_shape.push_back(other_half);
 	different_shapes.push_back(square_shape);
+	}
 
-	std::vector<std::vector<Tile>> long_shape;
-	std::vector<Tile> long_piece;
-	long_piece.push_back(Tile(0, 0, ofColor::lightBlue));
-	long_piece.push_back(Tile(0, Tile::height, ofColor::lightBlue));
-	long_piece.push_back(Tile(0, Tile::height * 2, ofColor::lightBlue));
-	long_piece.push_back(Tile(0, Tile::height * 3, ofColor::lightBlue));
-	long_shape.push_back(long_piece);
-	different_shapes.push_back(long_shape);
-
-	std::vector<std::vector<Tile>> t_shape;
+	void CreateT(){
+		std::vector<std::vector<Tile>> t_shape;
 	std::vector<Tile> right_t;
 	std::vector<Tile> left_t;
 	std::vector<Tile> middle_t;
@@ -38,7 +39,33 @@ void Shape::CreateShapes() {
 	t_shape.push_back(middle_t);
 	t_shape.push_back(right_t);
 	different_shapes.push_back(t_shape);
+	}
 
+	void CreateL(){
+	std::vector<std::vector<Tile>> l_shape;
+	std::vector<Tile> left_column;	
+	std::vector<Tile> right_column;
+	right_column.push_back(Tile(Tile::width, Tile::height * 2, ofColor::antiqueWhite));
+	right_column.push_back(Tile(0, 0, ofColor::antiqueWhite));
+	right_column.push_back(Tile(0, Tile::height, ofColor::antiqueWhite));
+	right_column.push_back(Tile(0, Tile::height * 2, ofColor::antiqueWhite));
+	l_shape.push_back(left_column);
+	l_shape.push_back(right_column);
+	different_shapes.push_back(l_shape);
+	}
+
+	void CreateLowerCasel() {
+	std::vector<std::vector<Tile>> long_shape;
+	std::vector<Tile> long_piece;
+	long_piece.push_back(Tile(0, 0, ofColor::lightBlue));
+	long_piece.push_back(Tile(0, Tile::height, ofColor::lightBlue));
+	long_piece.push_back(Tile(0, Tile::height * 2, ofColor::lightBlue));
+	long_piece.push_back(Tile(0, Tile::height * 3, ofColor::lightBlue));
+	long_shape.push_back(long_piece);
+	different_shapes.push_back(long_shape);
+	}
+
+	void CreateS(){
 	std::vector<std::vector<Tile>> right_shape;
 	std::vector<Tile> right_side_snake;
 	std::vector<Tile> left_side_snake;
@@ -51,8 +78,9 @@ void Shape::CreateShapes() {
 	right_shape.push_back(middle_side_snake);
 	right_shape.push_back(right_side_snake);
 	different_shapes.push_back(right_shape);
+	}
 
-
+	void CreateZ() {
 	std::vector<std::vector<Tile>> left_shape;
 	std::vector<Tile> other_right_side_snake;
 	std::vector<Tile> ohter_left_side_snake;
@@ -65,7 +93,9 @@ void Shape::CreateShapes() {
 	left_shape.push_back(other_middle_side_snake);
 	left_shape.push_back(other_right_side_snake);
 	different_shapes.push_back(left_shape);
+	}
 
+	void CreateJ(){
 	std::vector<std::vector<Tile>> j_shape;
 	std::vector<Tile> other_left_column;
 	std::vector<Tile> other_right_column;
@@ -76,18 +106,7 @@ void Shape::CreateShapes() {
 	j_shape.push_back(other_left_column);
 	j_shape.push_back(other_right_column);
 	different_shapes.push_back(j_shape);
-
-	std::vector<std::vector<Tile>> l_shape;
-	std::vector<Tile> left_column;	
-	std::vector<Tile> right_column;
-	right_column.push_back(Tile(Tile::width, Tile::height * 2, ofColor::antiqueWhite));
-	right_column.push_back(Tile(0, 0, ofColor::antiqueWhite));
-	right_column.push_back(Tile(0, Tile::height, ofColor::antiqueWhite));
-	right_column.push_back(Tile(0, Tile::height * 2, ofColor::antiqueWhite));
-	l_shape.push_back(left_column);
-	l_shape.push_back(right_column);
-	different_shapes.push_back(l_shape);
-}
+	}
 
 std::vector<std::vector<Tile>> Shape::get_random_shape() {
 	srand(time(NULL));
