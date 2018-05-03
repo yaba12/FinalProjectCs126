e0ea1bfe5b7c94a1556a42c92b9fbef0b6cf394e
yaba12 <31941014+yaba12@users.noreply.github.com>
2018-05-02 20:48:43 -0500

    Update Shape.h

--- src/Shape.h
+++ src/Shape.h
@@ -1,20 +1,21 @@
+
 #pragma once
 #include "Tile.h"
 #include <vector>
-
 class Shape {
+private: 
+	static std::vector<std::vector<std::vector<Tile>>> different_shapes;
+	static void CreateSquare();									//Creates all the shapes we use
+	static void CreateT();
+	static void CreateL();
+	static void CreateLowerCasel();
+	static void CreateS();
+	static void CreateZ();
+	static void CreateJ();
+		
 public:
-	std::vector<std::vector<std::vector<Tile>>> different_shapes;
-	
-	//Make the Shapes and add to vector
-	void CreateShapes();
-	void CreateSquare();
-	void CreateT();
-	void CreateL();
-	void CreateLowerCasel();
-	void CreateS();
-	void CreateZ();
-	void CreateJ();
-	std::vector < std::vector<Tile>> GetRandomShape();
-	void draw(std::vector<std::vector<Tile>> shape);
+	static void CreateShapes();									//Generates list of different shapes
+	static std::vector < std::vector<Tile>> GetRandomShape();	//Returns a randomly generated shape
+	static void Draw(std::vector<std::vector<Tile>> shape);		//Draws the shape
+	static void Move(std::vector<std::vector<Tile>> shape);		//Moves the shape
 };
