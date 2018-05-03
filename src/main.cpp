966a45d7351fd9b560f790c66e586e191700a7b3
yaba12 <31941014+yaba12@users.noreply.github.com>
2018-05-02 20:49:27 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-	ofSetFrameRate(12);
+	ofSetupOpenGL(1200,2000,OF_WINDOW);			// <-------- setup the GL context
+	ofSetFrameRate(5);
 	// this kicks off the running of my app
 	// can be OF_WINDOW or OF_FULLSCREEN
 	// pass in width and height too:
